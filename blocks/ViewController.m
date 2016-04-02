d62cf31dcb0e64e158f77958366e7232abcaca88
surendra kumar <ksurendrakumar01@gmail.com>
2016-04-02 09:04:35 +0530

    post

--- blocks/ViewController.m
+++ blocks/ViewController.m
@@ -1,15 +1,14 @@
 //
 //  ViewController.m
-//  blocks
+//  postingCOUNTRYCLUB
 //
-//  Created by surendra kumar k on 13/03/16.
+//  Created by surendra kumar k on 02/04/16.
 //  Copyright © 2016 frendsOrg. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
-- (IBAction)get:(id)sender;
 
 @end
 
@@ -17,41 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
-    NSString *make = @"kumar";
-    NSString *(^getFullName)(NSString *) = ^
-    
-    (NSString *model)
-    
-    {
-        return [make stringByAppendingFormat:@" %@", model];
-    };
-    
-    
-    
-    NSLog(@"%@", getFullName(@"surendra"));
-
-    // Honda Accord
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-
-
-
-
-
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)get:(id)sender {
-    
-    
-
-    
-}
 @end
